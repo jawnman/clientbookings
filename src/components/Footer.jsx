8df84bbce35be4d21ba39936ff74fcfe1b9fcf81
Nick Roehm <nickroehmxyz@gmail.com>
2026-02-13 11:38:13 -0500

    Add TikTok link to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,6 +14,15 @@ const socialLinks = [
         ),
     },
     {
+        name: 'TikTok',
+        href: 'https://www.tiktok.com/@nick.roehm',
+        icon: (
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                <path d="M9 12a4 4 0 1 0 4 4V4a5 5 0 0 0 5 5" />
+            </svg>
+        ),
+    },
+    {
         name: 'Email',
         href: 'mailto:nickroehmxyz@gmail.com',
         icon: (
